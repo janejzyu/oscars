7ec9cfeff512ec24a025471666a3c307ec01fa9c
janejzyu <yujiangzhen@hotmail.com>
2016-11-25 11:13:15 -0500

    release date added

--- code/main.r
+++ code/main.r
@@ -2,19 +2,43 @@ rm(list = ls())
 
 this.dir = dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-
-load("../data/IMDBdata.RData")
-#metascore = NULL
-for(i in 386:nrow(IMDBdata)) {
+library(magrittr)
+library(rvest)
+#metascore
+load("IMDBdata.RData")
+metascore = NULL
+for(i in 1:nrow(IMDBdata)) {
   print(i)
   link = as.character(IMDBdata[i,]$movie_imdb_link)
   score = read_html(link) %>%
-          html_nodes(".metacriticScore span") %>%
-          html_text %>%
-          as.numeric()
+    html_nodes(".metacriticScore span") %>%
+    html_text %>%
+    as.numeric()
   if (identical(score, numeric(0))) {
     score = NA
   }
   metascore = c(metascore, score)
 }
 
+
+IMDBdata$metascore = metascore
+write.csv(IMDBdata, "IMDBdata.csv", row.names=FALSE)
+save(IMDBdata, file = "IMDBdata.RData")
+
+releasedate = NULL
+for(i in 1:nrow(IMDBdata)) {
+  print(paste(i, "iteration"))
+  
+  link = as.character(IMDBdata[i,]$movie_imdb_link)
+  txt = read_html(link) %>% 
+    html_nodes(xpath = "//div/h4[contains(text(),'Release Date:')]/..") %>%
+    html_text()
+  
+  date = gsub("\\n.*", "",gsub(".*Release Date: ", "", txt))
+  if (identical(date, character(0))) {
+    date = NA
+  }
+  releasedate = c(releasedate, date)
+}
+IMDBdata$releasedate = releasedate
+save(IMDBdata, file = "IMDBdata.RData")
