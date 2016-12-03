a89fcc104301d741010151c4436907a57818b673
janejzyu <yujiangzhen@hotmail.com>
2016-12-03 11:42:34 -0500

    golden globes data scraped from wiki

---
+++ code/goldenglobesScrap.R
@@ -0,0 +1,16 @@
+rm(list = ls())
+library(rvest)
+
+this.dir = dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+
+html = read_html("https://en.wikipedia.org/wiki/Golden_Globe_Award_for_Best_Director")
+t2000 = (html %>% 
+         html_nodes(xpath = "//*[@id='mw-content-text']/table[8]") %>%
+         html_table())[[1]]
+t2010 = (html %>% 
+         html_nodes(xpath = "//*[@id='mw-content-text']/table[9]") %>%
+         html_table())[[1]]
+
+globesScrap = rbind(t2000, t2010)
+save(globesScrap, file = "../data/globesScrap.RData")
