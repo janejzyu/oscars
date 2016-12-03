ef99c51d05d560655f0223bdcd54752a9ccf795e
janejzyu <yujiangzhen@hotmail.com>
2016-12-03 00:02:32 -0500

    move misplaced merged data

--- code/merge.R
+++ code/merge.R
@@ -12,4 +12,4 @@ load("../data/IMDBdata_nomiss.RData")
 load("../data/golden_globes.RData")
 
 IMDB_globes = merge(IMDBdata, subset(globes, Category=="Director"), by.x = "title", by.y = "Film.Title")
-save(IMDB_globes, file = "IMDB_globes.RData")
+save(IMDB_globes, file = "../data/IMDB_globes.RData")
