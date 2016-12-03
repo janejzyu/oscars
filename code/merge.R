dfe04aa29e2584763aa4e30086402aea202cb79d
janejzyu <yujiangzhen@hotmail.com>
2016-12-03 09:50:02 -0500

    split release date

--- code/merge.R
+++ code/merge.R
@@ -12,4 +12,10 @@ load("../data/IMDBdata_nomiss.RData")
 load("../data/golden_globes.RData")
 
 IMDB_globes = merge(IMDBdata, subset(globes, Category=="Director"), by.x = "title", by.y = "Film.Title")
+
+releaseInfo = matrix(unlist(strsplit(IMDB_globes$releasedate, " ")), ncol = 4, byrow = TRUE)
+IMDB_globes$r_month = releaseInfo[,2]
+IMDB_globes$r_year = as.numeric(releaseInfo[,3])
+IMDB_globes$r_location = gsub(".*\\((.*)\\).*", "\\1", releaseInfo[,4])
+
 save(IMDB_globes, file = "../data/IMDB_globes.RData")
