ddeb976b62bc41a4bacfcb6bac5acb04986aa73b
janejzyu <yujiangzhen@hotmail.com>
2016-12-02 23:59:43 -0500

    merged globes (Director only) and IMDB

--- code/merge.R
+++ code/merge.R
@@ -10,5 +10,6 @@ load("../data/IMDBdata_nomiss.RData")
 #globes = globes[, 1:13] #remove extra NA columns
 #save(globes, file = "../data/golden_globes.RData")
 load("../data/golden_globes.RData")
-IMDB_globes = merge(IMDBdata, globes, by.x = "title", by.y = "Film.Title")
+
+IMDB_globes = merge(IMDBdata, subset(globes, Category=="Director"), by.x = "title", by.y = "Film.Title")
 save(IMDB_globes, file = "IMDB_globes.RData")
