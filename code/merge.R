410021dbde6fa7e78cb1b62015762ad57c672758
janejzyu <yujiangzhen@hotmail.com>
2016-12-02 23:51:53 -0500

    merged globes and IMDB

---
+++ code/merge.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+library(gdata)
+this.dir = dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+
+
+load("../data/IMDBdata_nomiss.RData")
+
+#globes = read.xls("../data/Golden_Globes_2006.xls", sheet = "Golden_Globes", header = TRUE)
+#globes = globes[, 1:13] #remove extra NA columns
+#save(globes, file = "../data/golden_globes.RData")
+load("../data/golden_globes.RData")
+IMDB_globes = merge(IMDBdata, globes, by.x = "title", by.y = "Film.Title")
+save(IMDB_globes, file = "IMDB_globes.RData")
