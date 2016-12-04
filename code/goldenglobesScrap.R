d2a8f580a61c2b13ed4e6ac0b13cf26dd96113f1
janejzyu <yujiangzhen@hotmail.com>
2016-12-03 22:15:23 -0500

    merge and edit release info"

--- code/goldenglobesScrap.R
+++ code/goldenglobesScrap.R
@@ -20,30 +20,34 @@ t2010 = (html %>%
          html_table())[[1]]
 colnames(t1990) = colnames(t2000)
 
+#clean data
 globesScrap = rbind(t1990, t2000, t2010)
 globesScrap = subset(globesScrap, Year >= 1997)
 globesScrap$`Winners and nominees` = sub(" †| ‡", "", globesScrap$`Winners and nominees`)
+#status: 1 nominee; 2 winner
+globesScrap$GGstatus = 1
+globesScrap$GGstatus[match(unique(globesScrap$Year), globesScrap$Year)] = 2
 
-globesScrap$isWinner = FALSE
-globesScrap$isWinner[match(unique(globesScrap$Year), globesScrap$Year)] = TRUE
-
+colnames(globesScrap) = c("GGyear", "film", "director", "GGstatus")
 save(globesScrap, file = "../data/globesScrap.RData")
 
 
 #academy awards best director
 
 html = read_html("https://en.wikipedia.org/wiki/Academy_Award_for_Best_Director")
-aa = (html %>% 
+aaScrap = (html %>% 
       html_nodes(xpath = "//*[@id='mw-content-text']/table[3]") %>%
       html_table())[[1]]
-aa$Year = substring(aa$Year, 1, 4)
-aa = subset(aa, Year >= 1997)
-aa = aa[ , !(names(aa) %in% c("Ref."))]
-aa$`Director(s)` = sub(".*!", "", aa$`Director(s)`)
-aa$Film = sub(".*!", "", aa$Film)
-aa$isWinner = FALSE
-aa$isWinner[match(unique(aa$Year), aa$Year)] = TRUE
+#clean data
+aaScrap$Year = substring(aaScrap$Year, 1, 4)
+aaScrap = subset(aaScrap, Year >= 1997)
+aaScrap = aaScrap[ , !(names(aaScrap) %in% c("Ref."))]
+aaScrap$`Director(s)` = sub(".*!", "", aaScrap$`Director(s)`)
+aaScrap$Film = sub(".*!", "", aaScrap$Film)
+#status: 1 nominee; 2 winner
+aaScrap$AAstatus = 1
+aaScrap$AAstatus[match(unique(aaScrap$Year), aaScrap$Year)] = 2
 
-
-
-save(aa, file = "../data/aaScrap.RData")
+aaScrap = aaScrap[,c(1,3,2,4)]
+colnames(aaScrap) = c("AAyear", "film", "director", "AAstatus")
+save(aaScrap, file = "../data/aaScrap.RData")
--- code/merge.R
+++ code/merge.R
@@ -4,18 +4,44 @@ this.dir = dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 
-load("../data/IMDBdata_nomiss.RData")
+load("../data/IMDBdata_yearupdated.Rdata")
 
+#ya-shin http://www.ya-shin.com/awards/awards.html
 #globes = read.xls("../data/Golden_Globes_2006.xls", sheet = "Golden_Globes", header = TRUE)
 #globes = globes[, 1:13] #remove extra NA columns
 #save(globes, file = "../data/golden_globes.RData")
-load("../data/golden_globes.RData")
+#load("../data/golden_globes.RData")
 
-IMDB_globes = merge(IMDBdata, subset(globes, Category=="Director"), by.x = "title", by.y = "Film.Title")
+load("../data/globesScrap.RData")
+load("../data/aaScrap.RData")
 
-releaseInfo = matrix(unlist(strsplit(IMDB_globes$releasedate, " ")), ncol = 4, byrow = TRUE)
-IMDB_globes$r_month = releaseInfo[,2]
-IMDB_globes$r_year = as.numeric(releaseInfo[,3])
-IMDB_globes$r_location = gsub(".*\\((.*)\\).*", "\\1", releaseInfo[,4])
+IMDB_globes = merge(IMDBdata, globesScrap[,c("GGyear", "film", "GGstatus")], by.x = "title", by.y = "film", all = TRUE)
+#IMDB_globes = merge(IMDBdata, globesScrap[,c("film", "GGstatus")], by.x = "title", by.y = "film", all.x = TRUE)
+#IMDB_globes = merge(IMDB_globes, aaScrap[,c("film", "AAstatus")], by.x = "title", by.y = "film", all = TRUE)
+
+IMDB_globes$GGstatus[is.na(IMDB_globes$GGstatus)] = 0
+#IMDB_globes$AAstatus[is.na(IMDB_globes$AAstatus)] = 0
+
+IMDB_globes = IMDB_globes[order(IMDB_globes$r_year), ]
+
+
+t = data.frame(IMDB_globes$title, IMDB_globes$movie_imdb_link, IMDB_globes$year, IMDB_globes$r_year, IMDB_globes$GGyear, IMDB_globes$GGstatus, IMDB_globes$r_month, IMDB_globes$country)
+t = na.omit(t)
+t = t[which((t$IMDB_globes.r_year - t$IMDB_globes.GGyear) != 0), ]
+
+t$IMDB_globes.r_month = 12
+t$IMDB_globes.r_month[which(t$IMDB_globes.title == "Elizabeth")] = 10
+t$IMDB_globes.r_month[which(t$IMDB_globes.title == "The Artist")] = 11
+
+t$IMDB_globes.country = "USA"
+t$IMDB_globes.r_year = t$IMDB_globes.r_year - 1
+
+
+for(i in 1:nrow(t)) {
+  j = which(IMDB_globes$movie_imdb_link == t[i, ]$IMDB_globes.movie_imdb_link)
+  IMDB_globes[j, "r_month"] = t[i, ]$IMDB_globes.r_month
+  IMDB_globes[j, "county"] = t[i, ]$IMDB_globes.country
+  IMDB_globes[j, "r_year"] = t[i, ]$IMDB_globes.r_year
+}
 
 save(IMDB_globes, file = "../data/IMDB_globes.RData")
