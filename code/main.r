6e6bdf7c195975670ab96431107ff75bfa41003a
janejzyu <yujiangzhen@hotmail.com>
2016-12-04 13:00:17 -0500

    add genre indicator

--- code/main.r
+++ code/main.r
@@ -3,7 +3,15 @@ library(rvest)
 
 this.dir = dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-load("../data/IMDBdata_MisAnalysed.Rdata")
+load("../data/IMDBmerged_cleanedalldates.Rdata")
 
 genres = unique(unlist(strsplit(as.character(IMDBdata$genres),"\\|")))
 #as.data.frame(table(unlist(strsplit(as.character(IMDBdata$genres),"\\|"))))
+
+getIndicator = function(genList) {
+  sapply(genres, grepl, genList)
+}
+indicators = t(as.matrix(sapply(IMDBdata$genres, getIndicator)))
+IMDBdata = cbind(IMDBdata, indicators)
+
+save(IMDBdata, file = "../data/IMDBdata_gIndicator.RData")
